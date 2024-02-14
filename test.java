62e700b35c60b40c8eec7cefb2e1d7ed5d455f61
Eden Dubrovsky <ed239@njit.edu>
2024-02-14 17:53:35 -0500

    First Commit, Test Repo
    Printing hello

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1a
+		System.out.print("Hello, this is a test of our repo");
+
+    }
+
+}
