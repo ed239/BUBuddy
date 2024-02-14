81fb59a1111f1c930dca31755a2703ee430f3324
Bolatbek Amiyev <bolatbekamiev@gmail.com>
2024-02-14 18:06:36 -0500

    added new line and .ignore

--- test.java
+++ test.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 1a
 		System.out.print("Hello, this is a test of our repo");
+		System.out.println("This is the added Bolatbek line");
 
     }
 
