8d3bb3ea70e7fed7477c0ee050e11e612cd5b4fd
Eden Dubrovsky <ed239@njit.edu>
2024-03-02 19:38:26 -0500

    Created client and server

--- chatApp/src/main/java/org/chatapp/BUBuddyApp.java
+++ chatApp/src/main/java/org/chatapp/BUBuddyApp.java
@@ -7,6 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import org.chatapp.network.chatClient;
+import org.chatapp.network.chatServer;
+
+
 public class BUBuddyApp extends Application {
     private final int sceneWidth = 1100;
     private final int sceneHeight = 700;
@@ -14,12 +18,23 @@ public class BUBuddyApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BUBuddyApp.class.getResource("LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), sceneWidth, sceneHeight);
+
+        //creates new client each time your run
+        SceneController sceneController = fxmlLoader.getController();
+        chatClient newChatClient = new chatClient(sceneController);
+        sceneController.setChatClient(newChatClient);
+
+
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        //comment these two out if running them independently
+        new Thread(chatServer::main).start();
+        new Thread(chatClient::main).start();
         launch();
     }
 }
