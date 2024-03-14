cf692a295a76e3f1a0440ed741ba7e2b7f534a00
Eden Dubrovsky <ed239@njit.edu>
2024-03-14 10:27:05 -0400

    Authentication and database working solution
    I added a sample layout of the sample login and sign up page for testing purposes. Connected to the db and checked that log in works

--- chatApp/src/main/java/org/chatapp/BUBuddyApp.java
+++ chatApp/src/main/java/org/chatapp/BUBuddyApp.java
@@ -19,13 +19,14 @@ public class BUBuddyApp extends Application {
     private final int sceneHeight = 700;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BUBuddyApp.class.getResource("LoginPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), sceneWidth, sceneHeight);
+        FXMLLoader fxmlLoader = new FXMLLoader(BUBuddyApp.class.getResource("LogInPage2.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), sceneWidth, sceneHeight);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 450);
 
         //creates new client each time your run
         SceneController sceneController = fxmlLoader.getController();
         chatClient newChatClient = new chatClient(sceneController);
-        sceneController.setChatClient(newChatClient);
+//        sceneController.setChatClient(newChatClient);
 
 
 
--- chatApp/src/main/java/org/chatapp/SceneController.java
+++ chatApp/src/main/java/org/chatapp/SceneController.java
@@ -9,8 +9,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import org.chatapp.com.mongodb.Database;
 import org.chatapp.network.chatClient;
 
 public class SceneController {
@@ -22,52 +24,70 @@ public class SceneController {
 
     private chatClient client;
 
-    public void setChatClient(chatClient client) {
-        this.client = client;
-    }
-
-
-
+//    public void setChatClient(chatClient client) {
+//        this.client = client;
+//    }
+    @FXML
+    private Label lblcurrentuser;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("JavaFX Application!");
+    private TextField txtusername;
+
+    @FXML
+    private TextField txtpassword;
+
+    @FXML
+    private TextField txtfullname;
+    Database database = Database.getInstance();
+    public void Login(ActionEvent event) throws IOException{
+        // Provide username and password
+        String username = txtusername.getText();
+        String password = txtpassword.getText();
+
+        boolean exists = database.userExists(username);
+        if (exists) {
+            System.out.println("User exists.");
+            boolean validUser = database.verifyPassword(username, password);
+            if(validUser){
+                System.out.println("Authenticated!");
+            }else{
+                System.out.println("Not Authenticated");
+            }
+
+        } else {
+            System.out.println("User does not exist.");
+        }
+
     }
 
     public void loginPageToSignUpPage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SignUpPage.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(root);
-        stage.show();
-    }
-
-    public void loginPageToChatPage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ChatPage.fxml")));
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SignUp2.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.getScene().setRoot(root);
         stage.show();
 
     }
 
-    public void chatPageToLoginPage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginPage.fxml")));
+    public void signUp(ActionEvent event) throws IOException {
+        String fullname = txtfullname.getText();
+        String username = txtusername.getText();
+        String password = txtpassword.getText();
+
+        database.createUser(fullname, username, password);
+    }
+
+    public void backToLogIn(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LogInPage2.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.getScene().setRoot(root);
         stage.show();
     }
 
-    public void signUpPageToLoginPage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginPage.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(root);
-        stage.show();
-    }
-
-    public void signUpPageToLoginPageNoAccountCreated(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginPage.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(root);
-        stage.show();
-    }
-
+    //    public void loginPageToChatPage(ActionEvent event) throws IOException {
+//        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ChatPage.fxml")));
+//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        stage.getScene().setRoot(root);
+//        stage.show();
+//
+//    }
 }
