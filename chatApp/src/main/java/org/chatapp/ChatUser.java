7855c9db96de6dee8064de6d4ba85e9f2d427e1a
Eden Dubrovsky <ed239@njit.edu>
2024-03-20 11:13:36 -0400

    adding dob to backend

--- chatApp/src/main/java/org/chatapp/ChatUser.java
+++ chatApp/src/main/java/org/chatapp/ChatUser.java
@@ -5,11 +5,14 @@ public class ChatUser {
     private ObjectId id;
     private String name;
     private String username;
+    private String dob;
 
-    public ChatUser(ObjectId id, String name, String username) {
+    public ChatUser(ObjectId id, String name, String username, String dateOfBirth) {
         this.id = id;
         this.name = name;
         this.username = username;
+        this.dob = dateOfBirth;
+
     }
 
     public ObjectId getId() {
@@ -23,5 +26,7 @@ public class ChatUser {
     public String getUsername() {
         return username;
     }
+
+    public String getDOB() { return dob;}
 }
 
