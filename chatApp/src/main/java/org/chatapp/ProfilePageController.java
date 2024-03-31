624782c9e8ee6662b21d89988c64f037fc7f6796
AZAMAT OMIRZAK <146376666+AZAMAT-GIT-HUB@users.noreply.github.com>
2024-03-30 22:29:41 -0400

    Added upload photo file

--- chatApp/src/main/java/org/chatapp/ProfilePageController.java
+++ chatApp/src/main/java/org/chatapp/ProfilePageController.java
@@ -1,15 +1,23 @@
 package org.chatapp;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class ProfilePageController {
+public class ProfilePageController implements Initializable {
+    @FXML
+    private ImageView imageView;
     public void backToChatPage(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ChatPage.fxml")));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -17,4 +25,9 @@ public class ProfilePageController {
         stage.show();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image image = new Image(getClass().getResourceAsStream("/img/pfofilephoto.png"));
+        imageView.setImage(image);
+    }
 }
