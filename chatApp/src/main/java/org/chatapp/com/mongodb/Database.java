2bb36550f85513af4ebbda1e12f643a4da6b6844
Eden Dubrovsky <ed239@njit.edu>
2024-04-18 18:25:45 -0400

    Uri

--- chatApp/src/main/java/org/chatapp/com/mongodb/Database.java
+++ chatApp/src/main/java/org/chatapp/com/mongodb/Database.java
@@ -34,6 +34,7 @@ public class Database {
     private Database() {
         // Replace string with our db connection
         ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("org.mongodb.driver").setLevel(Level.ERROR); //disables logs from mongoDB
+        //Change uri here
         String uri = "";
         MongoClient mongoClient = MongoClients.create(uri);
         MongoDatabase database = mongoClient.getDatabase("sample_chat");
