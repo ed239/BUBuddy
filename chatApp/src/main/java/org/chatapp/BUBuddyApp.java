39361ae07110721fb8e1d3d9e45df60233b076d0
Bolatbek Amiyev <bolatbekamiev@gmail.com>
2024-04-22 14:28:36 -0400

    comments added

--- chatApp/src/main/java/org/chatapp/BUBuddyApp.java
+++ chatApp/src/main/java/org/chatapp/BUBuddyApp.java
@@ -14,7 +14,17 @@ import java.io.IOException;
 //  Authors: Eden Dubrovsky, Jacob Kustra, Bolatbek Amiyev, Azamat Omirzak
 //  Date: Spring 2024
 //  Class: MET CS622
-//  Issues: None known
+//  Issues:
+//        1. Problem with resizing window by mouse
+//        2. After changing the name in the Profile, the application must be re-launched so that others can see the new name
+//        3. If you click the logout button, the client socket will not close
+//        4. If the client couldn't connect to the server socket, the application will start because the main chat works with MongoDB,
+//        but if the client wants to connect to the server, he needs to restart the application
+//        5. If the user goes to Profile page and back to Chat page, in the local chat, other users must send the message twice to be seen,
+//        and only every second message can be viewed. The number of times the user will go to Profile and back to Chat page,
+//        the number of times they(other users) will need to send messages to be seen. This problem is not with the socket, but with JavaFX and Thread.
+//        6. Separating ChatContainer for different chats
+//   All this is doable, it just takes time
 //
 //
 
